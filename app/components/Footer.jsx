efcb227e85493af147e21a9d8da59a1dcce19212
Nasir Johnson <nasir11buckets@icloud.com>
2025-10-28 15:57:28 -0500

    Removed header icons from cart aside and fixed opening and closing cart transitions

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -70,7 +70,7 @@ export function Footer({footer: footerPromise, header, publicStoreDomain}) {
                     display: 'flex',
                     alignItems: 'center',
                     backgroundColor: 'transparent',
-                    border: '1px solid rgba(255, 255, 255, 0.3)',
+                    border: '2px solid rgba(255, 255, 255, 0.3)',
                     borderRadius: '39px',
                     overflow: 'hidden'
                   }}>
