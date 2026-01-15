122765ce2d1b48236f44e56d83595d581fe9d32e
Nasir Johnson <nasir11buckets@icloud.com>
2026-01-14 23:26:11 -0600

    Add valid GraphQL placeholder for Hydrogen codegen

--- app/graphql/customer-account/placeholder.js
+++ app/graphql/customer-account/placeholder.js
@@ -1,1 +1,11 @@
-// Placeholder file to satisfy Hydrogen GraphQL codegen
+import {graphql} from '@shopify/hydrogen';
+
+/**
+ * Placeholder GraphQL document to satisfy Hydrogen codegen.
+ * This will be replaced with real customer account queries later.
+ */
+export const CUSTOMER_ACCOUNT_PLACEHOLDER = graphql(`
+  fragment CustomerAccountPlaceholder on Customer {
+    id
+  }
+`);
